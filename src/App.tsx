dae4955f58c627ec14c835d31aa41d2f760a015e
sazzad2saad <sazzad2saad@gmail.com>
2026-01-12 04:14:06 +0600

    2.0 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useEffect, useState } from "react";
 import Portfolio from "./Portfolio";
 
 function App() {
-  const [dark, setDark] = useState(true);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", dark);
-  }, [dark]);
-
-  return <Portfolio dark={dark} setDark={setDark} />;
+  return <Portfolio />;
 }
 
 export default App;
